210d619a4f5d8a16e6c157ce08e41a1ef9ab0f34
Anik Kumar Das <akd.prince07@gmail.com>
2022-04-19 13:10:06 +0600

    flag done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Country from './components/Country/Country';
 
 function App() {
+  const [countries, setCountries]= useState([]);
+  useEffect(()=> {
+    fetch("https://restcountries.com/v2/all")
+    .then(res => res.json())
+    .then(data => {
+      setCountries(data);
+    })
+    .catch(error => console.log(error));
+  }, [])
   return (
     <div className="App">
+      <h2>Country loaded: {countries.length}</h2>
+      <ul>
+        {
+          countries.map(country => <Country country={country} key={country.alpha3Code}></Country>)
+        }
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
