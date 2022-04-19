c5d867d2a471290b0e7b5f2ee42bca0195c47b29
Anik Kumar Das <akd.prince07@gmail.com>
2022-04-20 01:14:21 +0600

    flag done with border

--- src/App.js
+++ src/App.js
@@ -13,27 +13,20 @@ function App() {
     })
     .catch(error => console.log(error));
   }, [])
+  const handleAddCountry = (country) => {
+    console.log(country)
+  };
   return (
     <div className="App">
       <h2>Country loaded: {countries.length}</h2>
+      <h2>Country Added: </h2>
       <ul>
         {
-          countries.map(country => <Country country={country} key={country.alpha3Code}></Country>)
+          countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
         }
       </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import './Country.css';
 
 const Country = (props) => {
     const {name,population,region,flag} = props.country;
+    const handleAddCountry = props.handleAddCountry;
     
     return (
-        <div>
+        <div className='country'>
             <h1>This is: {name}</h1>
             <h2>This Country Population is: {population}</h2>
             <h2><small>This is in : {region} Region</small></h2>
-            <h1 height="20px" width="20px">Flag: <img src={flag} alt="" /></h1>
+            <h3>Flag: <img style={{height:"80px"}} src={flag} alt="" /></h3>
+            <button onClick={() => handleAddCountry(props.country)}>Add Country</button>
+            
         </div>
     );
 };
